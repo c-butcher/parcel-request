1730c2955ee719326125721bcc45ea468b93a95f
Chris Butcher <c.butcher@hotmail.com>
2017-04-06 20:38:33 -0700

    Turned the Request class into a singleton.

--- src/Request.php
+++ src/Request.php
@@ -11,4 +11,40 @@ namespace Parcel\Request;
  */
 class Request extends RequestBase {
 
+	/**
+	 * Singleton instance of this class.
+	 *
+	 * @var self
+	 */
+	protected static $_Instance = null;
+
+	/**
+	 * Request constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+
+		if ( self::$_Instance === null ) {
+			self::$_Instance = $this;
+
+		} else {
+			/**
+			 * @todo We should populate this new instance with data from the existing instance.
+			 */
+		}
+	}
+
+	/**
+	 * Returns the singleton instance of this class.
+	 *
+	 * @return Request
+	 */
+	public static function &GetInstance()
+	{
+		if ( self::$_Instance === null  || ! is_object( self::$_Instance ) ) {
+			self::$_Instance = new self();
+		}
+
+		return self::$_Instance;
+	}
 }
